9372465bf5820e1e786f86901389d971cb51f0ab
Caio Ribeiro <caio.ribeiro98@outlook.com>
2021-04-20 19:09:28 -0300

    Adiciona ouvidor de eventos do button no HTML

--- Open community/script/scripts.js
+++ Open community/script/scripts.js
@@ -8,59 +8,13 @@ button1.addEventListener("click", function() {
         alert("Por favor, preencha todos os campos obrigatórios")
     } else {
         alert("Cadastro feito com sucesso! Redirecionando...")
-        window.location.href = "home.html"
+        window.location.href = "https://www.braziljs.org/"
 
     }
 })
 
 function aviso2() {
 
-    button2.addEventListener("click", function() {
-        let name_login = document.querySelector(".entrances_name")
-        let password_login = document.querySelector(".entrances_password")
-
-        if (name_login.value == '' || password_login.value == '') {
-            alert("Nome ou Senha não preenchidos")
-        } else {
-            alert("Redirecionando...")
-            window.location.href = "home.html"
-        }
-    })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function aviso() {
-
-    document.querySelector(".registers_button")
-    let email_box = document.querySelector(".email")
-    let name_box = document.querySelector(".name")
-    if (email_box.value == '' || name_box.value == '') {
-        alert("Por favor, preencha todos os campos obrigatórios")
-    } else {
-        alert("Cadastro feito com sucesso! Redirecionando...")
-        window.location.href = "home.html"
-
-    }
-}
-
-function aviso2() {
-    document.querySelector(".entrances_button")
     let name_login = document.querySelector(".entrances_name")
     let password_login = document.querySelector(".entrances_password")
 
@@ -68,12 +22,7 @@ function aviso2() {
         alert("Nome ou Senha não preenchidos")
     } else {
         alert("Redirecionando...")
-        window.location.href = "home.html"
+        window.location.href = "https://www.braziljs.org/"
     }
 
 }
-
-function load() {
-    document.getElementsByTagName("body")
-    alert("Bem vindo a comunidade aberta do BrazilJS, No menu superior clicando em Open comunnity você será direcionado para a página oficial e clicando em um dos links no canto superior direito, você obtém mais informações sobre a comunidade")
-}*/
