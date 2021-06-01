33527a47a97d3fac2be2445757ad2b98c535f0cd
Caio Ribeiro <caio.ribeiro98@outlook.com>
2021-06-01 16:42:22 -0300

    Adiciona validação no texto do ícone dark/light

--- login_page/index.js
+++ login_page/index.js
@@ -1,5 +1,6 @@
 let visibility_on = window.document.querySelector('#visibility_icons')
 let type_password = window.document.querySelector('.password')
+let theme = window.document.querySelector('#theme_icon')
 
 function show_password() {
     if (type_password.type === 'password') {
@@ -9,4 +10,14 @@ function show_password() {
         type_password.type = 'password'
         visibility_on.innerHTML = 'visibility_off'
     }
+}
+
+function foo_theme() {
+    if (theme.innerHTML === 'light_mode' && theme.id === 'theme_icon') {
+        theme.innerHTML = 'dark_mode'
+        theme.setAttribute('id', 'changeForDark')
+    } else {
+        theme.innerHTML = 'light_mode'
+    }
+
 }
