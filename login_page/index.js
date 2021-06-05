8ec4cdb438f6481ec6e2f5d1b7b67ad17b02f917
Caio Ribeiro <caio.ribeiro98@outlook.com>
2021-06-05 11:02:33 -0300

    comenta seletor da checkbox(linha 62)

--- login_page/index.js
+++ login_page/index.js
@@ -3,16 +3,7 @@ let type_password = window.document.querySelector('.password')
 let theme = window.document.querySelector('#light_on')
 let theme_container = window.document.querySelector('.light_container')
 let body = window.document.querySelector('.body_light')
-
-function show_password() {
-    if (type_password.type === 'password') {
-        type_password.type = 'text';
-        visibility_on.innerHTML = 'visibility'
-    } else {
-        type_password.type = 'password'
-        visibility_on.innerHTML = 'visibility_off'
-    }
-}
+let checkbox = window.document.querySelector('.check')
 
 function foo_theme() {
     if (theme.innerHTML === 'light_mode') {
@@ -28,3 +19,53 @@ function foo_theme() {
     }
 
 }
+
+function show_password() {
+    if (type_password.type === 'password') {
+        type_password.type = 'text';
+        visibility_on.innerHTML = 'visibility'
+    } else {
+        type_password.type = 'password'
+        visibility_on.innerHTML = 'visibility_off'
+    }
+}
+
+/* Função para armazenar os dados do usuário 
+no armazenamento local do navegador: */
+
+/* function save_user() {
+    let email = document.querySelector('.name');
+    let password = document.querySelector('.password');
+
+    let dados = localStorage.getItem('user_data');
+
+    if (dados === null) {
+        localStorage.setItem('user_data', '[]');
+        dados = [];
+
+    }
+
+    let auxRegistro = {
+        nome: email.value,
+        senha: password.value
+    }
+
+    dados.push(auxRegistro);
+
+    localStorage.setItem('user_data', JSON.stringify(dados));
+}
+let dados = localStorage.getItem('user_data').value
+*/
+
+/* Função para buscar os dados do usuário 
+no armazenamento local do navegador: */
+
+/* let email = document.querySelector('.name');
+let password = document.querySelector('.password');
+
+function load_data() {
+    if (checkbox.checked === true) {
+        email.text = localStorage.getItem('user_data')
+        password.text = localStorage.getItem('user_data')
+    }
+} */
