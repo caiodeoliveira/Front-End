8ebc31097fc1e188513c15699cafc2d2f37e2b45
Caio Ribeiro <caio.ribeiro98@outlook.com>
2021-06-05 11:03:20 -0300

    comenta seletor da checkbox(linha 62)

--- login_page/index.js
+++ login_page/index.js
@@ -3,7 +3,6 @@ let type_password = window.document.querySelector('.password')
 let theme = window.document.querySelector('#light_on')
 let theme_container = window.document.querySelector('.light_container')
 let body = window.document.querySelector('.body_light')
-let checkbox = window.document.querySelector('.check')
 
 function foo_theme() {
     if (theme.innerHTML === 'light_mode') {
@@ -36,22 +35,22 @@ no armazenamento local do navegador: */
 /* function save_user() {
     let email = document.querySelector('.name');
     let password = document.querySelector('.password');
-
+    
     let dados = localStorage.getItem('user_data');
-
+    
     if (dados === null) {
         localStorage.setItem('user_data', '[]');
         dados = [];
 
     }
-
+    
     let auxRegistro = {
         nome: email.value,
         senha: password.value
     }
-
+    
     dados.push(auxRegistro);
-
+    
     localStorage.setItem('user_data', JSON.stringify(dados));
 }
 let dados = localStorage.getItem('user_data').value
@@ -60,7 +59,8 @@ let dados = localStorage.getItem('user_data').value
 /* Função para buscar os dados do usuário 
 no armazenamento local do navegador: */
 
-/* let email = document.querySelector('.name');
+/*let checkbox = window.document.querySelector('.check')
+ let email = document.querySelector('.name');
 let password = document.querySelector('.password');
 
 function load_data() {
